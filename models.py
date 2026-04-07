f0cbde231c89b9472addc36261aa65f45bce70f9
Vaishnavi <103645367+Vaishnavvii@users.noreply.github.com>
2026-04-07 23:07:06 +0530

    Add SQLAlchemy models for Product, Warehouse, and Inventory

--- models.py
+++ models.py
@@ -1,1 +1,20 @@
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    sku = db.Column(db.String(50), unique=True)
+    price = db.Column(db.Numeric)
+
+class Warehouse(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+class Inventory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer)
+    warehouse_id = db.Column(db.Integer)
+    quantity = db.Column(db.Integer)
